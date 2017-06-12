dc95fcb41af7c298615a2da3738d9933ae3db300
paupiszc <pauliska7@gmail.com>
2017-06-12 20:44:41 +0200

    To jest nowe repozytowium w którym będzie zawarta gra z repozytorium MemoryGame_Paulina.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,6 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MemoryGame";
             this.ResumeLayout(false);
 
